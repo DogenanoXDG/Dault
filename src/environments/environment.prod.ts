f14a42a92ff80513d2dde7621c80ef0749d1965b
Dogenano-xdg <103167736+Dogenano-xdg@users.noreply.github.com>
2023-05-31 03:34:37 +0200

    Update Reps in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -30,19 +30,39 @@ export const environment = {
     }
   ],
   representativeAccounts: [
+    'xdg_1france7tsdxw3r9jfe5kur4b1nayd8b8wknqbccy8owfgs765dn98rkwojd', 
+    'xdg_1e4ecrhmcws6kwiegw8dsbq5jstq7gqj7fspjmgiu11q55s6xnsnp3t9jqxf',
     'xdg_1mz6am1zez5y8u87s7pkddagkag4xtub7bktn8wm6o5taekwypfww4p6frby', // XDG 1
+    'xdg_1kxzwigqegun4a4xw5yjgpk99iqgxt5uftd5d6e6h4omemyexbu3cmtmm4sa',
+    'xdg_1sa6qhb31u7y8hqkz3p5egc1buyoe6k8pyr1kgzifwhw9yzuhu3i4k5etzfg'
   ],
   defaultRepresentatives: [
     {
-      id: 'xdg_1mz6am1zez5y8u87s7pkddagkag4xtub7bktn8wm6o5taekwypfww4p6frby',
-      name: 'Dogenano Official',
+      id: 'xdg_1france7tsdxw3r9jfe5kur4b1nayd8b8wknqbccy8owfgs765dn98rkwojd',
+      name: 'MyEcoria France',
       warn: false,
     },
     {
-      id: 'xdg_3trcizdrj8uck5f46onn3tomac7wgx5rfey7a1y5dp9q3dqyn6ccup688atx',
-      name: 'MyEcoria',
+      id: 'xdg_1e4ecrhmcws6kwiegw8dsbq5jstq7gqj7fspjmgiu11q55s6xnsnp3t9jqxf',
+      name: 'Nanswap',
       warn: false,
-    }
+    },
+    {
+      id: 'xdg_1mz6am1zez5y8u87s7pkddagkag4xtub7bktn8wm6o5taekwypfww4p6frby',
+      name: 'DogeNano #1',
+      warn: false,
+    },
+    {
+      id: 'xdg_1kxzwigqegun4a4xw5yjgpk99iqgxt5uftd5d6e6h4omemyexbu3cmtmm4sa',
+      name: 'DogeNano #2',
+      warn: false,
+    },
+    {
+      id: 'xdg_1sa6qhb31u7y8hqkz3p5egc1buyoe6k8pyr1kgzifwhw9yzuhu3i4k5etzfg',
+      name: 'DogeNano #3',
+      warn: false,
+    },    
+
   ],
   donationAddress: 'xdg_3dauit3e9fqzooonwxfio8rbe9m7a3pjen9qis5e4nqtzqd5qdp7ekhsdjcf', 
 };
